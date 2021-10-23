66ac67567bf0b6a0574d24bfd863f7115b4d41e8
riflan <riflan0ahmed@gmail.com>
2021-10-23 22:40:55 +0530

    feat: added storybookjs

--- src/pages/Books/Books.tsx
+++ src/pages/Books/Books.tsx
@@ -40,7 +40,7 @@ const Books = () => {
   }, [fetchBooks]);
 
   return (
-    <div className="w-full h-screen p-10 bg-gray-100">
+    <div className="w-full p-10 bg-gray-100">
       <Header />
       <Body />
     </div>
