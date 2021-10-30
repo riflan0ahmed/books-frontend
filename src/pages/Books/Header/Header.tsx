93fef969129d71b65695d400d78d1f7ec4a0073e
riflan <riflan0ahmed@gmail.com>
2021-10-30 15:06:40 +0530

    refactor: added breadcrumb

--- src/pages/Books/Header/Header.tsx
+++ src/pages/Books/Header/Header.tsx
@@ -2,6 +2,7 @@ import { Button } from "@mui/material";
 import { useCallback } from "react";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router";
+import Breadcrumb from "stories/Surface/Breadcrumb/Breadcrumb";
 import { updateModalCreate } from "utils/bookSlice";
 
 const Header = () => {
@@ -19,7 +20,9 @@ const Header = () => {
 
   return (
     <div className="flex flex-row w-full my-8">
-      <div className="flex justify-start my-auto mr-auto">Pagination</div>
+      <div className="flex justify-start my-auto mr-auto">
+        <Breadcrumb />
+      </div>
       <div className="flex justify-end gap-5 ml-auto">
         <Button color="primary" variant="contained" onClick={open}>
           Create
---
+++ src/stories/Surface/Breadcrumb/Breadcrumb.tsx
@@ -0,0 +1,38 @@
+import { RouteComponentProps, withRouter } from "react-router";
+import { Breadcrumbs, Link, Typography } from "@mui/material";
+import React from "react";
+
+type SomeComponentProps = RouteComponentProps;
+
+const Breadcrumb: React.FC<SomeComponentProps> = ({ history, location }) => {
+  const pathname = location.pathname;
+  const pathNames = pathname.split("/").filter((x: any) => x);
+  return (
+    <div role="presentation">
+      <Breadcrumbs>
+        <Link onClick={() => history.push("/")}>Home</Link>
+        {pathNames.map((name: string, index: number) => {
+          let routeTo = `/${pathNames.slice(0, index + 1).join("/")}`;
+          let isLast = index !== pathNames.length - 1;
+
+          return isLast ? (
+            // <Link onclick={() => history.push(routeTo)}>{name}</Link>
+
+            <Link
+              onClick={() => history.push(routeTo)}
+              style={{ textTransform: "capitalize" }}
+            >
+              {name}
+            </Link>
+          ) : (
+            <Typography style={{ textTransform: "capitalize" }}>
+              {name}
+            </Typography>
+          );
+        })}
+      </Breadcrumbs>
+    </div>
+  );
+};
+
+export default withRouter(Breadcrumb);
