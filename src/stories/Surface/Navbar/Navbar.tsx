50eb7685d3322f080e500a4834d885f0bf50e33b
riflan <riflan0ahmed@gmail.com>
2021-11-03 18:19:33 +0530

    refactor: added color for navbar

--- src/stories/Surface/Navbar/Navbar.tsx
+++ src/stories/Surface/Navbar/Navbar.tsx
@@ -1,14 +1,20 @@
+import { blue } from "@mui/material/colors";
+
 interface NavbarProps {
   label: string;
 }
 
 const Navbar = (props: NavbarProps) => {
   const { label } = props;
+  const color = blue[500];
 
   return (
-    <div className="grid w-full grid-cols-12 p-5 bg-white">
+    <div
+      className="grid w-full grid-cols-12 p-5"
+      style={{ backgroundColor: color }}
+    >
       <div className="col-span-2 my-auto">
-        <label className="text-lg font-semibold text-gray-800">{label}</label>
+        <label className="text-lg font-semibold text-white">{label}</label>
       </div>
       <div className="col-span-10"></div>
     </div>
